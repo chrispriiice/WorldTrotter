894a3e4913b45b8d369be38e908fbebb2050e6cc
Chris Price <christopher.r.price@gmail.com>
2020-01-14 23:29:54 -0500

    Inital commit. Starting over from scratch.

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Chris Price on 1/14/20.
+//  Copyright © 2020 Chris Price. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
